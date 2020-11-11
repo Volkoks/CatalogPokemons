b4424aa204f33555a38343f44ee46a4a57b44067
Volkoks <volk-2390@gmail.com>
2020-11-11 22:27:44 +0300

    Тестовый репозиторий

---
+++ app/src/main/java/com/example/catalogpokemons/data/repository/PokemonRepo.kt
@@ -0,0 +1,12 @@
+package com.example.catalogpokemons.data.repository
+
+import com.example.catalogpokemons.data.entity.Pokemon
+
+class PokemonRepo {
+
+    val localRepo = listOf{
+        Pokemon("Пикачу")
+        Pokemon("Бульбазабр")
+        Pokemon("Сквидвард")
+    }
+}
