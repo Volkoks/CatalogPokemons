012dd8f4a33587ecdfc950226c3a12ee95f9919f
Volkoks <volk-2390@gmail.com>
2020-11-11 22:36:41 +0300

    Тестовый репозиторий

--- app/src/main/java/com/example/catalogpokemons/data/repository/PokemonRepo.kt
+++ app/src/main/java/com/example/catalogpokemons/data/repository/PokemonRepo.kt
@@ -1,12 +1,17 @@
 package com.example.catalogpokemons.data.repository
 
 import com.example.catalogpokemons.data.entity.Pokemon
+import io.reactivex.rxjava3.core.Observable
 
 class PokemonRepo {
 
-    val localRepo = listOf{
-        Pokemon("Пикачу")
-        Pokemon("Бульбазабр")
+    private val localRepo = listOf(
+        Pokemon("Пикачу"),
+        Pokemon("Бульбазабр"),
         Pokemon("Сквидвард")
+    )
+//        Метод для получения листа через Rx
+    fun getPokemon(): Observable<List<Pokemon>> {
+        return Observable.fromIterable(listOf(localRepo))
     }
 }
