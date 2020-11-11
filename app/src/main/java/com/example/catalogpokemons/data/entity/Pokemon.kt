46e317c53ba15e5c5d328c36435c3f846fd212b7
Volkoks <volk-2390@gmail.com>
2020-11-11 22:24:22 +0300

    ЗАкоментировал параметры покемона для тестировки остальных компонентов приложения пока не тяну данные из сети

--- app/src/main/java/com/example/catalogpokemons/data/entity/Pokemon.kt
+++ app/src/main/java/com/example/catalogpokemons/data/entity/Pokemon.kt
@@ -5,10 +5,11 @@ import kotlinx.android.parcel.Parcelize
 
 @Parcelize
 data class Pokemon(
-    var base_experience: Float,
-    var height: Float,
-    var id: Int,
-    var name:String,
-    var order:Float
-):Parcelable {
+//    var base_experience: Float,
+//    var height: Float,
+//    var id: Int,
+    var name: String,
+//    var order: Float,
+//    val sprites: Sprites
+) : Parcelable {
 }
