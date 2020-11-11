3db327a50ab64d8f35485fc48682a47d2b3c1718
Volkoks <volk-2390@gmail.com>
2020-11-11 20:48:47 +0300

    Сделал класс Покемона

---
+++ app/src/main/java/com/example/catalogpokemons/data/entity/Pokemon.kt
@@ -0,0 +1,14 @@
+package com.example.catalogpokemons.data.entity
+
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
+data class Pokemon(
+    var base_experience: Float,
+    var height: Float,
+    var id: Int,
+    var name:String,
+    var order:Float
+):Parcelable {
+}
