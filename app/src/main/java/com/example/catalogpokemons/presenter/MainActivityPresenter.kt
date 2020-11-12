97fb15042da1a2cd6d0ed1dc72ba05746faaaa18
Volkoks <volk-2390@gmail.com>
2020-11-12 16:44:16 +0300

    Настроил начально MainActivityPresenter.kt

--- app/src/main/java/com/example/catalogpokemons/presenter/MainActivityPresenter.kt
+++ app/src/main/java/com/example/catalogpokemons/presenter/MainActivityPresenter.kt
@@ -1,11 +1,17 @@
 package com.example.catalogpokemons.presenter
 
+import com.example.catalogpokemons.R
+import com.example.catalogpokemons.app.PokemonApp
 import com.example.catalogpokemons.navigator.Screens
 import com.example.catalogpokemons.view.MainActivityView
 import moxy.MvpPresenter
 import ru.terrakok.cicerone.Router
+import ru.terrakok.cicerone.android.support.SupportAppNavigator
 
 class MainActivityPresenter(val router: Router): MvpPresenter<MainActivityView>() {
+
+
+
     override fun onFirstViewAttach() {
         super.onFirstViewAttach()
         router.replaceScreen(Screens.PokemonsScreen())
