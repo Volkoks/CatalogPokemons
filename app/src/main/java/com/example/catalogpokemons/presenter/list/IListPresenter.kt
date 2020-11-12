862fe560531c801b76a386fcb51a4ba185d418ec
Volkoks <volk-2390@gmail.com>
2020-11-12 13:56:41 +0300

    Изменил тип переменной для клика

--- app/src/main/java/com/example/catalogpokemons/presenter/list/IListPresenter.kt
+++ app/src/main/java/com/example/catalogpokemons/presenter/list/IListPresenter.kt
@@ -3,7 +3,7 @@ package com.example.catalogpokemons.presenter.list
 import com.example.catalogpokemons.view.IItemView
 
 interface IListPresenter<V : IItemView> {
-    val itemClickListener: ((V) -> Unit)
+    var itemClickListener: ((V) -> Unit)?
     fun getCount(): Int
     fun bind(view: V)
 }
