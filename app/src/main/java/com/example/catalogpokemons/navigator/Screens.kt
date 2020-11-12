1da6c0cede3ab0609935323c6abff3aeacc37337
Volkoks <volk-2390@gmail.com>
2020-11-12 17:17:30 +0300

    Добавил создание экрана покемона

--- app/src/main/java/com/example/catalogpokemons/navigator/Screens.kt
+++ app/src/main/java/com/example/catalogpokemons/navigator/Screens.kt
@@ -1,6 +1,9 @@
 package com.example.catalogpokemons.navigator
 
 
+import androidx.fragment.app.Fragment
+import com.example.catalogpokemons.data.entity.Pokemon
+import com.example.catalogpokemons.ui.fragments.PokemonFragment
 import com.example.catalogpokemons.ui.fragments.PokemonsFragment
 import ru.terrakok.cicerone.android.support.SupportAppScreen
 
@@ -8,4 +11,8 @@ class Screens {
     class PokemonsScreen : SupportAppScreen() {
         override fun getFragment() = PokemonsFragment.newInstance()
     }
-}
+    class PokemonScreen(val pokemon: Pokemon):SupportAppScreen(){
+        override fun getFragment() = PokemonFragment.newInstance(pokemon)
+        }
+    }
+
