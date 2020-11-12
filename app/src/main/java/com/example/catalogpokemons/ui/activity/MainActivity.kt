984283feecf2914d23b463758b73283fd760d384
Volkoks <volk-2390@gmail.com>
2020-11-12 16:44:01 +0300

    Настроил начально MainActivity.kt

--- app/src/main/java/com/example/catalogpokemons/ui/activity/MainActivity.kt
+++ app/src/main/java/com/example/catalogpokemons/ui/activity/MainActivity.kt
@@ -3,13 +3,33 @@ package com.example.catalogpokemons.ui.activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.catalogpokemons.R
+import com.example.catalogpokemons.app.PokemonApp
+import com.example.catalogpokemons.presenter.MainActivityPresenter
+import com.example.catalogpokemons.view.MainActivityView
+import moxy.MvpAppCompatActivity
+import moxy.ktx.moxyPresenter
+import ru.terrakok.cicerone.android.support.SupportAppNavigator
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : MvpAppCompatActivity(), MainActivityView {
 
+    private val presenter: MainActivityPresenter by moxyPresenter {
+        MainActivityPresenter(PokemonApp.instance.getRouter)
+    }
+    private val navigatorHolder = PokemonApp.instance.navigatorHolder
+    private val navigator = SupportAppNavigator(this, supportFragmentManager, R.id.container)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+    }
+    override fun onResumeFragments() {
+        super.onResumeFragments()
+        navigatorHolder.setNavigator(navigator)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        navigatorHolder.removeNavigator()
     }
 }
