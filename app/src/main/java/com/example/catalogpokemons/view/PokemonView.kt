651b542012fecd9305e73b8d61b95c3d74bfdb3c
Volkoks <volk-2390@gmail.com>
2020-11-12 08:48:11 +0300

    Сделал интерфесы для взаимодействия ui и presenter будущих классов

---
+++ app/src/main/java/com/example/catalogpokemons/view/PokemonView.kt
@@ -0,0 +1,10 @@
+package com.example.catalogpokemons.view
+
+import moxy.MvpView
+import moxy.viewstate.strategy.AddToEndSingleStrategy
+import moxy.viewstate.strategy.StateStrategyType
+
+@StateStrategyType(AddToEndSingleStrategy::class)
+interface PokemonView:MvpView {
+    fun init()
+}
